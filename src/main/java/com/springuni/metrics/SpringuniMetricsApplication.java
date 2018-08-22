d43372c661dc94d83a2b4837e8f4240bb48f62ce
Laszlo Csontos <github@laszlocsontos.com>
2018-08-22 08:26:47 +0200

    SUME-1 Initial commit

---
+++ src/main/java/com/springuni/metrics/SpringuniMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.springuni.metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringuniMetricsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringuniMetricsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/springuni/metrics/SpringuniMetricsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springuni.metrics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringuniMetricsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
